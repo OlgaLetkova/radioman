08e08a8b48fe2460a9b65f9cd46bceb01ffd030d
Olga Letkova <olja.let52@gmail.com>
2020-10-24 17:31:11 +0300

    Impruving Radioman with Constructor

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -2,54 +2,69 @@ package ru.netology.domain;
 
 public class Radio {
     private int numberOfCurrentRadioStation;
+    private int maxNumberOfRadioStation = 9;
     private int volumeOfSound;
+    private int minVolumeOfSound = 0;
+    private int maxVolumeOfSound = 100;
+
+    public Radio() {
+    }
+
+    public Radio(int maxQuantityOfRadioStation) {
+        this.maxNumberOfRadioStation = maxQuantityOfRadioStation-1;
+    }
 
     public int getNumberOfCurrentRadioStation() {
         return numberOfCurrentRadioStation;
     }
 
     public void setNumberOfCurrentRadioStation(int numberOfCurrentRadioStation) {
-        if (numberOfCurrentRadioStation > 9) {
+       if (numberOfCurrentRadioStation > this.maxNumberOfRadioStation) {
             return;
         }
         this.numberOfCurrentRadioStation = numberOfCurrentRadioStation;
     }
 
     public void nextNumberOfRadioStation() {
-        if (this.numberOfCurrentRadioStation < 9) {
+        if (this.numberOfCurrentRadioStation < this.maxNumberOfRadioStation) {
             this.numberOfCurrentRadioStation += 1;
         } else this.numberOfCurrentRadioStation = 0;
     }
 
     public void prevNumberOfRadioStation() {
         if (this.numberOfCurrentRadioStation == 0) {
-            this.numberOfCurrentRadioStation = 9;
+            this.numberOfCurrentRadioStation = this.maxNumberOfRadioStation;
         } else this.numberOfCurrentRadioStation -= 1;
     }
 
+    public Radio(int volumeOfSound,int minVolumeOfSound, int maxVolumeOfSound) {
+        this.minVolumeOfSound = minVolumeOfSound;
+        this.maxVolumeOfSound = maxVolumeOfSound;
+        this.volumeOfSound = volumeOfSound;
+    }
 
     public int getVolumeOfSound() {
         return volumeOfSound;
     }
 
     public void setVolumeOfSound(int volumeOfSound) {
-        if (volumeOfSound > 10) {
+        if (volumeOfSound > this.maxVolumeOfSound) {
             return;
         }
-        if (volumeOfSound < 0) {
+        if (volumeOfSound < this.minVolumeOfSound) {
             return;
         }
         this.volumeOfSound = volumeOfSound;
     }
 
     public void increaseVolumeOfSound() {
-        if (this.volumeOfSound < 10) {
+        if (this.volumeOfSound < this.maxVolumeOfSound) {
             this.volumeOfSound += 1;
         }
     }
 
     public void decreaseVolumeOfSound() {
-        if (this.volumeOfSound >0) {
+        if (this.volumeOfSound >this.minVolumeOfSound) {
             this.volumeOfSound -= 1;
         }
     }
